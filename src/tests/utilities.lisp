453678d5e5ebc6f19865da5f53e933c8d0cdea38
Mark Fedurin <hitecnologys@gmail.com>
2013-04-09 22:46:04 +0700

    Some minor variables changes (defparameter -> defconstant)

--- src/tests/utilities.lisp
+++ src/tests/utilities.lisp
@@ -1,20 +1,20 @@
 (in-package #:isol)
 
-(defparameter *test-objects-list-path* (make-pathname :directory '(:relative "res")
-                                                      :name "test-objects-list"
-                                                      :type "isol"))
-(defparameter *test-map-path*          (make-pathname :directory '(:relative "res")
-                                                      :name "test-map"
-                                                      :type "isol"))
+(defconstant +test-objects-list-path+ (make-pathname :directory '(:relative "res")
+                                                     :name "test-objects-list"
+                                                     :type "isol"))
+(defconstant +test-map-path+          (make-pathname :directory '(:relative "res")
+                                                     :name "test-map"
+                                                     :type "isol"))
 
 (defmacro with-test-objects-list ((variable) &body body)
   "Reads test objects file and binds the resilt to `variable'"
-  `(let ((,variable (load-objects-list-from-file *test-objects-list-path*)))
+  `(let ((,variable (load-objects-list-from-file +test-objects-list-path+)))
      ,@body))
 
 (defmacro with-test-map ((map-variable &optional (objects-variable (gensym))) &body body)
   `(with-test-objects-list (,objects-variable)
-     (let ((,map-variable (load-map-from-file *test-map-path* ,objects-variable)))
+     (let ((,map-variable (load-map-from-file +test-map-path+ ,objects-variable)))
        ,@body)))
 
 (defmacro with-test-rendered-map ((variable) &body body)
