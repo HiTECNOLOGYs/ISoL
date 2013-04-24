0d756250c573e75422c7a1b6820973d44bec1137
Mark Fedurin <hitecnologys@gmail.com>
2013-04-24 22:56:08 +0700

    Refactored some utility-functions code

--- src/base/utilities.lisp
+++ src/base/utilities.lisp
@@ -21,20 +21,20 @@
   #'(lambda (&rest arguments)
       (funcall function-1 (apply function-2 arguments))))
 
-(defun stream->list (stream &optional (s-expressions-processor nil processor-passed-p))
+(defun stream->list (stream &optional (s-expressions-processor nil processor-passed?))
   "Reads stream data to list of S-expressions."
-  (loop :for result = (read stream nil nil) :while result
-        :if processor-passed-p
-          :collecting (funcall s-expressions-processor result)
-        :else
-          :collecting result))
+  (loop for result = (read stream nil nil) while result
+        if processor-passed?
+          collecting (funcall s-expressions-processor result)
+        else
+          collecting result))
 
 (defun 2d-array->list (array)
   "Converts 2 dimensional array to list of lists whcih are rows of array."
   (destructuring-bind (size-y size-x) (array-dimensions array)
     (loop for i below size-y
-        collect (loop for j below size-x
-                      collect (aref array i j)))))
+          collect (loop for j below size-x
+                        collect (aref array i j)))))
 
 (defun list->string (list)
   "Converts list of characters to string."
