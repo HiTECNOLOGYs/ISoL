2176ac2e629edb72d8654af5e105b19511fb37b9
Mark Fedurin <hitecnologys@gmail.com>
2013-04-26 00:17:04 +0700

    Wrote ultra-simple wrapper for walking and viewing map

--- src/base/game.lisp
+++ src/base/game.lisp
@@ -1,9 +1,41 @@
 (in-package #:isol)
 
 (defstruct Game
-  (player nil)
-  (map nil))
+  map
+  (player (make-instance 'player :location (list 1 1)))
+  (creatures (make-hash-table)))
+
+(defun game-step (game)
+  (clear-screen)
+  (print-map (game-map game))
+  (write (game-player game)
+         :stream :game-window)
+  (redraw-screen)
+  (process-key (wait-for-key)
+               (game-player game)
+               (game-map game)))
+
+
+(defparameter *game* (make-game))
+
+(define-condition exit-game () ())
+
+(define-key-processor #\q ()
+  (declare (ignore player map))
+  (error 'exit-game))
 
 (defun run-game ()
   "Runs game."
-  nil)
+  (setf (game-map *game*)
+        (load-map-from-file (make-pathname :directory '(:relative "res")
+                                           :name "test-map"
+                                           :type "isol")))
+ (with-screen (:noecho)
+   (catch 'end-game
+     (handler-case (loop (game-step *game*)
+                         (sleep 1/100))
+       (exit-game ()
+         (throw 'end-game (values)))
+       (sb-sys:interactive-interrupt ()
+         (throw 'end-game (values)))))))
+         
