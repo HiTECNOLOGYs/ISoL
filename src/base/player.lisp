6b78ebc9df36f77567e719536448f823d9a55cc4
Mark Fedurin <hitecnologys@gmail.com>
2013-04-26 00:16:20 +0700

    Added some simple movements bindings

--- src/base/player.lisp
+++ src/base/player.lisp
@@ -10,3 +10,15 @@
       (incf (first (location player)) x)
       (incf (second (location player)) y)
       (list (first (location player)) (second (location player))))))
+
+(define-key-processor #\j ()
+  (move-creature player map 0 1))
+
+(define-key-processor #\k ()
+  (move-creature player map 0 -1))
+
+(define-key-processor #\h ()
+  (move-creature player map -1 0))
+
+(define-key-processor #\l ()
+  (move-creature player map 1 0))
