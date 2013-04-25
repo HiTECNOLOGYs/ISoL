a987f109d17118b0679260d8e933a736174a4e48
Mark Fedurin <hitecnologys@gmail.com>
2013-04-26 00:15:35 +0700

    Fixed player movement bug (screwed up with bindings)

--- src/base/player.lisp
+++ src/base/player.lisp
@@ -7,6 +7,6 @@
   "Moves player relative and checks map cell to passability."
   (destructuring-bind (player-x player-y) (location player)
     (when (map-cell-passable-p map (+ player-x x) (+ player-y y))
-      (incf player-x x)
-      (incf player-y y)
-      (list player-x player-y))))
+      (incf (first (location player)) x)
+      (incf (second (location player)) y)
+      (list (first (location player)) (second (location player))))))
