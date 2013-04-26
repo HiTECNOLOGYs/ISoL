1d654276b047d452eea4fb4c91f10197ccb5b071
Mark Fedurin <hitecnologys@gmail.com>
2013-04-26 17:35:21 +0700

    Changed player motion code and added function to pick up objects
    Binded picking to #\;

--- src/base/player.lisp
+++ src/base/player.lisp
@@ -17,11 +17,16 @@
 
 (defmethod move-creature ((player Player) map x y)
   "Moves player relative and checks map cell to passability."
+  (when (map-cell-passable-p map (+ (player-x player) x) (+ (player-y player) y))
+    (incf (player-x player) x)
+    (incf (player-y player) y)
+    (list (player-x player) (player-y player))))
+
+(defmethod pick-up-object ((player Player) map)
   (destructuring-bind (player-x player-y) (location player)
-    (when (map-cell-passable-p map (+ player-x x) (+ player-y y))
-      (incf (first (location player)) x)
-      (incf (second (location player)) y)
-      (list (first (location player)) (second (location player))))))
+    (when (takable-p (get-map-cell-top map player-x player-y))
+      (push (pop (get-map-cell-value map player-x player-y))
+            (inventory player)))))
 
 (define-key-processor #\j ()
   (move-creature player map 0 1))
@@ -34,3 +39,5 @@
 
 (define-key-processor #\l ()
   (move-creature player map 1 0))
+
+(define-key-binding #\; #'pick-up-object)
