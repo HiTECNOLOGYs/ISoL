cbc9de197a6ea17783e64fc8d7793d944f0a78ba
Mark Fedurin <hitecnologys@gmail.com>
2013-04-26 23:50:25 +0700

    New test for map stacks
    And fixed one stupid mistake in another test

--- src/tests/map-tests.lisp
+++ src/tests/map-tests.lisp
@@ -5,7 +5,7 @@
   `(is (eq (aref ,array ,y ,x) ,value)))
 
 (test test-symbol-conversion
-  (let ((instance (get-object-instance-from-symbol 'wall)))
+  (let ((instance (get-object-instance-from-symbol :wall)))
     (is (equal (name instance)
                "Wall"))
     (is (equal (description instance)
@@ -14,6 +14,23 @@
                10000))
     (is (eql (material instance)
              'stone))))
+
+(test test-objects-manipulations
+  (with-test-map (map)
+    (is (push-object map 1 1 (get-object-instance-from-symbol :gun)))
+    (is (and (eql (class-of (get-map-cell-top map 1 1))
+                  (class-of (make-instance 'Weapon)))
+             (equal (name (get-map-cell-top map 1 1))
+                    "Revolver")))
+    (is (push-object map 1 2 (get-object-instance-from-symbol :rock)))
+    (is (and (eql (class-of (get-map-cell-top map 1 2))
+                  (class-of (make-instance 'Map-Object)))
+             (equal (name (get-map-cell-top map 1 2))
+                    "Rock")))
+    (is (equal (name (pop-object map 1 1))
+               "Revolver"))
+    (is (equal (name (pop-object map 1 2))
+               "Rock"))))
 
 (test (test-map-loading :depends-on test-symbol-conversion)
   (with-test-map (map)
