3abafaa59b26afca8093b0a8a85647a41d0e77b6
Mark Fedurin <hitecnologys@gmail.com>
2014-04-14 23:39:08 +0700

    Implemented very simple main menu. It doesn't work properly, though

--- src/base/user-interaction.lisp
+++ src/base/user-interaction.lisp
@@ -21,3 +21,45 @@
   (let ((char (code-char (wait-for-key))))
     (second (assoc char choices
                    :test #'equal))))
+
+(defun draw-center-menu (window items)
+  "Draws some lines at the center of some window and highlights line
+  if it's selected. Format for items: (cons text selectd?)"
+  (destructuring-bind (x . y) (window-size window)
+    (iter
+      (for item in items)
+      (for print-y
+           from (- (ash y -1) (ash (length items) -1))
+           to   (+ (ash y -1) (ash (length items) -1)))
+      (destructuring-bind (item-text . selected?) item
+        (let ((print-x (- (ash x -1) (ash (length item-text) -1))))
+          (if selected?
+            (with-attributes (:underline)
+              (mvwprintw (or (window-ref window) *stdscr*) print-y print-x item-text))
+            (mvprintw print-y print-x item-text)))))))
+
+(defun display-center-menu (window-id &rest items)
+  "Displays menu where player can choise something by moving cursor up and down."
+  (let ((selected-item 0))
+    (clear-window-id window-id)
+    (labels ((move-selection-up ()
+               (when (< (1- (length items)) (incf selected-item))
+                 (setf selected-item (1- (length items))))
+               nil)
+             (move-selection-down ()
+               (when (> 0 (decf selected-item))
+                 (setf selected-item 0))
+               nil)
+             (select-item ()
+               selected-item))
+      (with-temporary-key-bindings ((#\j #'move-selection-down)
+                                    (#\k #'move-selection-up)
+                                    (#\Return #'select-item))
+        (iter
+          (for key next (wait-for-key))
+          (for result next (process-key key))
+          (until result)
+          (finally (return result))
+          (after-each (draw-center-menu (get-window-by-id window-id)
+                                        items)
+                      (redraw-window-id window-id)))))))
