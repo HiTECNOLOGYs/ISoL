e01ac89d7ed5062936d9c11f7dd2daaede13370d
Mark Fedurin <hitecnologys@gmail.com>
2014-07-17 15:53:24 +0700

    Wrote simple room generator and reduced size of map back to 10x10
    30x30 is too hard to debug in REPL. 10x10 should suffice.

--- src/base/map-generation.lisp
+++ src/base/map-generation.lisp
@@ -66,6 +66,14 @@ class by given generation rules."
   :hp 100000
   :material 'stone)
 
+(define-object-generator Door Map-Element
+  :name "Door"
+  :passable? t
+  :description "Scratched wooden door."
+  :display-character #\+
+  :hp 100
+  :material 'wood)
+
 (define-object-generator Gun Weapon
   :name "Revolver"
   :description "A bit rusty and dirty old revolver with no ammo."
@@ -74,9 +82,9 @@ class by given generation rules."
   :size 2
   :weight 150)
 
-(define-object-generator Rock Map-Object
-  :name "Rock"
-  :description "A hunge gray rock lying on the floor."
+(define-object-generator Boulder Map-Object
+  :name "Boulder"
+  :description "A hunge gray piece of rock lying on the floor."
   :movable? t
   :passable? t
   :display-character #\*
@@ -104,14 +112,15 @@ class by given generation rules."
     (:empty (gen-empty-map))))
 
 (defun gen-empty-map ()
-  (let ((map (make-array (list 30 30)
+  (let ((map (make-array (list 10 10)
                          :initial-element nil)))
     (set-map-borders map)))
 
 (defun gen-testing-map ()
   ;;; Generating sample map here for debugging purposes
   (let ((map (gen-empty-map)))
-    (push-object map 2 2 (generate-object 'Gun))
+    (push-object map 3 4 (generate-object 'Gun))
+    (gen-room map 2 2 :small :top 2)
     map))
 
 ;; ----------------
@@ -121,17 +130,42 @@ class by given generation rules."
 ;; simplicity. I know I'll have problems when I decide to go multi-core but I'm pretty
 ;; confident I can take care of it.
 
+(defun set-borders (map x1 y1 x2 y2)
+  (iter
+    (for i from y1 to y2)
+    (after-each
+      (push-object map x1 i (generate-object 'Wall))
+      (push-object map x2 i (generate-object 'Wall))))
+  (iter
+    (for i from (1+ x1) to (1- x2))
+    (after-each
+      (push-object map i y1 (generate-object 'Wall))
+      (push-object map i y2 (generate-object 'Wall))))
+  map)
+
 (defun set-map-borders (map)
   "Puts walls at the edges of the map."
   (destructuring-bind (y x) (array-dimensions map)
-    (iter
-      (for i from 0 to (1- y))
-      (after-each
-        (push-object map 0 i      (generate-object 'Wall))
-        (push-object map (1- x) i (generate-object 'Wall))))
-    (iter
-      (for i from 1 to (- x 2))
-      (after-each
-        (push-object map i 0      (generate-object 'Wall))
-        (push-object map i (1- y) (generate-object 'Wall)))))
+    (set-borders map 0 0 (1- x) (1- y)))
+  map)
+
+(defun gen-room (map x y type door-side door-position)
+  "Generates box of walls and sets door on appropriate space. It doesn't check "
+  (destructuring-bind (end-x end-y)
+      (ecase type
+        (:large (list (+ x 20) (+ y 20)))
+        (:standard (list (+ x 10) (+ y 10)))
+        (:small (list (+ x 5) (+ y 5))))
+    (when (or (<= (- end-x x) door-position)
+              (<= (- end-y y) door-position))
+      (error "Hey! Door position is bigger than the room."))
+    ;; Setting borders
+    (set-borders map x y end-x end-y)
+    ;; Making door
+    (let ((door (generate-object 'Door)))
+      (ecase door-side
+        (:right  (push-object map end-x (+ y door-position) door))
+        (:left   (push-object map x (+ y door-position) door))
+        (:top    (push-object map (+ x door-position) y door))
+        (:bottom (push-object map (+ y door-position) end-y door)))))
   map)
